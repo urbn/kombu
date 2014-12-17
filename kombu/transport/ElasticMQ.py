802ccc4f67164785693816ffcb7fab8d88127f17
David Esterkin <david.esterkin@gmail.com>
2014-12-17 14:04:47 -0500

    Fixed ElasticMQ version.

--- kombu/transport/ElasticMQ.py
+++ kombu/transport/ElasticMQ.py
@@ -2,7 +2,7 @@
 kombu.transport.ElasticMQ
 ===================
 
-TODO: Rewrite this for ElasticMQ!
+TODO:  Clean this up!
 Amazon SQS transport module for Kombu. This package implements an AMQP-like
 interface on top of Amazons SQS service, with the goal of being optimized for
 high performance and reliability.
@@ -44,20 +44,21 @@ import collections
 import socket
 import string
 
-from amqp.promise import transform, ensure_promise, promise
-from boto.sqs import SQSRegionInfo
+from anyjson import loads, dumps
 
-from kombu.async import get_event_loop
-from kombu.async.aws import sqs as _asynsqs
-from kombu.async.aws.ext import boto, exception
-from kombu.async.aws.sqs.connection import AsyncSQSConnection, SQSConnection
-from kombu.async.aws.sqs.ext import regions
-from kombu.async.aws.sqs.message import Message
-from kombu.five import Empty, range, string_t, text_t
+import boto
+from boto import exception
+from boto import sdb as _sdb
+from boto import sqs as _sqs
+from boto.sdb.domain import Domain
+from boto.sdb.connection import SDBConnection
+from boto.sqs.connection import SQSConnection
+from boto.sqs.message import Message
+
+from kombu.five import Empty, range, text_t
 from kombu.log import get_logger
-from kombu.utils import cached_property
+from kombu.utils import cached_property, uuid
 from kombu.utils.encoding import bytes_to_str, safe_str
-from kombu.utils.json import loads, dumps
 from kombu.transport.virtual import scheduling
 
 from . import virtual
@@ -66,35 +67,117 @@ logger = get_logger(__name__)
 
 # dots are replaced by dash, all other punctuation
 # replaced by underscore.
-CHARS_REPLACE_TABLE = {
-    ord(c): 0x5f for c in string.punctuation if c not in '-_.'
-}
+CHARS_REPLACE_TABLE = dict((ord(c), 0x5f)
+                           for c in string.punctuation if c not in '-_.')
 CHARS_REPLACE_TABLE[0x2e] = 0x2d  # '.' -> '-'
-
-#: SQS bulk get supports a maximum of 10 messages at a time.
-SQS_MAX_MESSAGES = 10
-
 
 def maybe_int(x):
     try:
         return int(x)
     except ValueError:
         return x
+BOTO_VERSION = tuple(maybe_int(part) for part in boto.__version__.split('.'))
+W_LONG_POLLING = BOTO_VERSION >= (2, 8)
+
+#: SQS bulk get supports a maximum of 10 messages at a time.
+SQS_MAX_MESSAGES = 10
+
+
+class Table(Domain):
+    """Amazon SimpleDB domain describing the message routing table."""
+    # caches queues already bound, so we don't have to declare them again.
+    _already_bound = set()
+
+    def routes_for(self, exchange):
+        """Iterator giving all routes for an exchange."""
+        return self.select("""WHERE exchange = '%s'""" % exchange)
+
+    def get_queue(self, queue):
+        """Get binding for queue."""
+        qid = self._get_queue_id(queue)
+        if qid:
+            return self.get_item(qid)
+
+    def create_binding(self, queue):
+        """Get binding item for queue.
+
+        Creates the item if it doesn't exist.
+
+        """
+        item = self.get_queue(queue)
+        if item:
+            return item, item['id']
+        id = uuid()
+        return self.new_item(id), id
+
+    def queue_bind(self, exchange, routing_key, pattern, queue):
+        if queue not in self._already_bound:
+            binding, id = self.create_binding(queue)
+            binding.update(exchange=exchange,
+                           routing_key=routing_key or '',
+                           pattern=pattern or '',
+                           queue=queue or '',
+                           id=id)
+            binding.save()
+            self._already_bound.add(queue)
+
+    def queue_delete(self, queue):
+        """delete queue by name."""
+        self._already_bound.discard(queue)
+        item = self._get_queue_item(queue)
+        if item:
+            self.delete_item(item)
+
+    def exchange_delete(self, exchange):
+        """Delete all routes for `exchange`."""
+        for item in self.routes_for(exchange):
+            self.delete_item(item['id'])
+
+    def get_item(self, item_name):
+        """Uses `consistent_read` by default."""
+        # Domain is an old-style class, can't use super().
+        for consistent_read in (False, True):
+            item = Domain.get_item(self, item_name, consistent_read)
+            if item:
+                return item
+
+    def select(self, query='', next_token=None,
+               consistent_read=True, max_items=None):
+        """Uses `consistent_read` by default."""
+        query = """SELECT * FROM `%s` %s""" % (self.name, query)
+        return Domain.select(self, query, next_token,
+                             consistent_read, max_items)
+
+    def _try_first(self, query='', **kwargs):
+        for c in (False, True):
+            for item in self.select(query, consistent_read=c, **kwargs):
+                return item
+
+    def get_exchanges(self):
+        return list(set(i['exchange'] for i in self.select()))
+
+    def _get_queue_item(self, queue):
+        return self._try_first("""WHERE queue = '%s' limit 1""" % queue)
+
+    def _get_queue_id(self, queue):
+        item = self._get_queue_item(queue)
+        if item:
+            return item['id']
 
 
 class Channel(virtual.Channel):
+    Table = Table
+
     default_region = 'us-east-1'
     default_visibility_timeout = 1800  # 30 minutes.
-    default_wait_time_seconds = 10  # disabled see #198
+    default_wait_time_seconds = 0  # disabled see #198
     domain_format = 'kombu%(vhost)s'
-    _asynsqs = None
+    _sdb = None
     _sqs = None
     _queue_cache = {}
     _noack_queues = set()
 
     def __init__(self, *args, **kwargs):
-        if boto is None:
-            raise ImportError('boto is not installed')
         super(Channel, self).__init__(*args, **kwargs)
 
         # SQS blows up when you try to create a new queue if one already
@@ -102,8 +185,11 @@ class Channel(virtual.Channel):
         # the queue_cache to protect us from recreating
         # queues that are known to already exist.
         queues = self.sqs.get_all_queues(prefix=self.queue_name_prefix)
+        logger.debug('There are %s queues' % len(queues))
         for queue in queues:
+            logger.debug('Next queue: %s' % queue)
             self._queue_cache[queue.name] = queue
+        self._fanout_queues = set()
 
         # The drain_events() method stores extra messages in a local
         # Deque object. This allows multiple messages to be requested from
@@ -111,13 +197,9 @@ class Channel(virtual.Channel):
         # to the caller of the drain_events() method.
         self._queue_message_cache = collections.deque()
 
-        self.hub = kwargs.get('hub') or get_event_loop()
-
     def basic_consume(self, queue, no_ack, *args, **kwargs):
         if no_ack:
             self._noack_queues.add(queue)
-        if self.hub:
-            self._loop1(queue)
         return super(Channel, self).basic_consume(
             queue, no_ack, *args, **kwargs
         )
@@ -175,8 +257,6 @@ class Channel(virtual.Channel):
 
     def _new_queue(self, queue, **kwargs):
         """Ensure a queue with given name exists in SQS."""
-        if not isinstance(queue, string_t):
-            return queue
         # Translate to SQS name for consistency with initial
         # _queue_cache population.
         queue = self.entity_name(self.queue_name_prefix + queue)
@@ -188,10 +268,56 @@ class Channel(virtual.Channel):
             )
             return q
 
+    def queue_bind(self, queue, exchange=None, routing_key='',
+                   arguments=None, **kwargs):
+        super(Channel, self).queue_bind(queue, exchange, routing_key,
+                                        arguments, **kwargs)
+        if self.typeof(exchange).type == 'fanout':
+            self._fanout_queues.add(queue)
+
+    def _queue_bind(self, *args):
+        """Bind ``queue`` to ``exchange`` with routing key.
+
+        Route will be stored in SDB if so enabled.
+
+        """
+        if self.supports_fanout:
+            self.table.queue_bind(*args)
+
+    def get_table(self, exchange):
+        """Get routing table.
+
+        Retrieved from SDB if :attr:`supports_fanout`.
+
+        """
+        if self.supports_fanout:
+            return [(r['routing_key'], r['pattern'], r['queue'])
+                    for r in self.table.routes_for(exchange)]
+        return super(Channel, self).get_table(exchange)
+
+    def get_exchanges(self):
+        if self.supports_fanout:
+            return self.table.get_exchanges()
+        return super(Channel, self).get_exchanges()
+
     def _delete(self, queue, *args):
         """delete queue by name."""
+        if self.supports_fanout:
+            self.table.queue_delete(queue)
         super(Channel, self)._delete(queue)
         self._queue_cache.pop(queue, None)
+
+    def exchange_delete(self, exchange, **kwargs):
+        """Delete exchange by name."""
+        if self.supports_fanout:
+            self.table.exchange_delete(exchange)
+        super(Channel, self).exchange_delete(exchange, **kwargs)
+
+    def _has_queue(self, queue, **kwargs):
+        """Return True if ``queue`` was previously declared."""
+        if self.supports_fanout:
+            return bool(self.table.get_queue(queue))
+        return super(Channel, self)._has_queue(queue)
 
     def _put(self, queue, message, **kwargs):
         """Put message onto queue."""
@@ -200,8 +326,29 @@ class Channel(virtual.Channel):
         m.set_body(dumps(message))
         q.write(m)
 
+    def _put_fanout(self, exchange, message, routing_key, **kwargs):
+        """Deliver fanout message to all queues in ``exchange``."""
+        for route in self.table.routes_for(exchange):
+            self._put(route['queue'], message, **kwargs)
+
+    def _get_from_sqs(self, queue, count=1):
+        """Retrieve messages from SQS and returns the raw SQS message objects.
+
+        :returns: List of SQS message objects
+
+        """
+        q = self._new_queue(queue)
+        if W_LONG_POLLING and queue not in self._fanout_queues:
+            return q.get_messages(
+                count, wait_time_seconds=self.wait_time_seconds,
+            )
+        else:  # boto < 2.8
+            return q.get_messages(count)
+
     def _message_to_python(self, message, queue_name, queue):
-        payload = loads(bytes_to_str(message.get_body()))
+        body = bytes_to_str(message.get_body())
+        logger.info('The body is %s' % body)
+        payload = loads(body)
         if queue_name in self._noack_queues:
             queue.delete_message(message)
         else:
@@ -226,33 +373,36 @@ class Channel(virtual.Channel):
         q = self._new_queue(queue)
         return [self._message_to_python(m, queue, q) for m in messages]
 
-    def _get_bulk(self, queue,
-                  max_if_unlimited=SQS_MAX_MESSAGES, callback=None):
+    def _get_bulk(self, queue, max_if_unlimited=SQS_MAX_MESSAGES):
         """Try to retrieve multiple messages off ``queue``.
 
-        Where :meth:`_get` returns a single Payload object, this method
-        returns a list of Payload objects.  The number of objects returned
-        is determined by the total number of messages available in the queue
-        and the number of messages the QoS object allows (based on the
+        Where _get() returns a single Payload object, this method returns a
+        list of Payload objects. The number of objects returned is determined
+        by the total number of messages available in the queue and the
+        number of messages that the QoS object allows (based on the
         prefetch_count).
 
         .. note::
-
             Ignores QoS limits so caller is responsible for checking
             that we are allowed to consume at least one message from the
             queue.  get_bulk will then ask QoS for an estimate of
             the number of extra messages that we can consume.
 
-        :param queue: The queue name to pull from.
-        :returns list: of message objects.
+        args:
+            queue: The queue name (string) to pull from
+
+        returns:
+            payloads: A list of payload objects returned
         """
         # drain_events calls `can_consume` first, consuming
         # a token, so we know that we are allowed to consume at least
         # one message.
-        maxcount = self._get_message_estimate()
+        maxcount = self.qos.can_consume_max_estimate()
+        maxcount = max_if_unlimited if maxcount is None else max(maxcount, 1)
         if maxcount:
-            q = self._new_queue(queue)
-            messages = q.get_messages(num_messages=maxcount)
+            messages = self._get_from_sqs(
+                queue, count=min(maxcount, SQS_MAX_MESSAGES),
+            )
 
             if messages:
                 return self._messages_to_python(messages, queue)
@@ -260,68 +410,11 @@ class Channel(virtual.Channel):
 
     def _get(self, queue):
         """Try to retrieve a single message off ``queue``."""
-        q = self._new_queue(queue)
-        messages = q.get_messages(num_messages=1)
+        messages = self._get_from_sqs(queue, count=1)
+
         if messages:
             return self._messages_to_python(messages, queue)[0]
         raise Empty()
-
-    def _loop1(self, queue, _=None):
-        self.hub.call_soon(self._schedule_queue, queue)
-
-    def _schedule_queue(self, queue):
-        if queue in self._active_queues:
-            if self.qos.can_consume():
-                self._get_bulk_async(
-                    queue, callback=promise(self._loop1, (queue, )),
-                )
-            else:
-                self._loop1(queue)
-
-    def _get_message_estimate(self, max_if_unlimited=SQS_MAX_MESSAGES):
-        maxcount = self.qos.can_consume_max_estimate()
-        return min(
-            max_if_unlimited if maxcount is None else max(maxcount, 1),
-            max_if_unlimited,
-        )
-
-    def _get_bulk_async(self, queue,
-                        max_if_unlimited=SQS_MAX_MESSAGES, callback=None):
-        maxcount = self._get_message_estimate()
-        if maxcount:
-            return self._get_async(queue, maxcount, callback=callback)
-        # Not allowed to consume, make sure to notify callback..
-        callback = ensure_promise(callback)
-        callback([])
-        return callback
-
-    def _get_async(self, queue, count=1, callback=None):
-        q = self._new_queue(queue)
-        return self._get_from_sqs(
-            q, count=count, connection=self.asynsqs,
-            callback=transform(self._on_messages_ready, callback, q, queue),
-        )
-
-    def _on_messages_ready(self, queue, qname, messages):
-        if messages:
-            callbacks = self.connection._callbacks
-            for raw_message in messages:
-                message = self._message_to_python(raw_message, qname, queue)
-                callbacks[qname](message)
-
-    def _get_from_sqs(self, queue,
-                      count=1, connection=None, callback=None):
-        """Retrieve and handle messages from SQS.
-
-        Uses long polling and returns :class:`~amqp.promise`.
-
-        """
-        connection = connection if connection is not None else queue.connection
-        return connection.receive_message(
-            queue, number_messages=count,
-            wait_time_seconds=self.wait_time_seconds,
-            callback=callback,
-        )
 
     def _restore(self, message,
                  unwanted_delivery_info=('sqs_message', 'sqs_queue')):
@@ -359,7 +452,7 @@ class Channel(virtual.Channel):
 
     def close(self):
         super(Channel, self).close()
-        for conn in (self._sqs, self._asynsqs):
+        for conn in (self._sqs, self._sdb):
             if conn:
                 try:
                     conn.close()
@@ -373,11 +466,15 @@ class Channel(virtual.Channel):
                 if _r.name == self.region:
                     return _r
 
+    #TODO: Remove all of the region discovery code
     def _aws_connect_to(self, fun, regions):
+        from boto.sqs import SQSRegionInfo
+        logger.debug('conninfo is: {userid: %s, password: %s, hostname: %s, port: %s' %
+                     (self.conninfo.userid, self.conninfo.password , self.conninfo.hostname, self.conninfo.port))
         conninfo = self.conninfo
+        region = self._get_regioninfo(regions)
         region_name = '%s:%s' % (self.conninfo.hostname, self.conninfo.port)
-        region = SQSRegionInfo(connection = SQSConnection, name = region_name, endpoint = self.conninfo.hostname) 
-        #TODO: make is_secure configurable. Is that already in the conninfo?
+        region = SQSRegionInfo(connection=SQSConnection, name=region_name, endpoint=self.conninfo.hostname)
         return fun(is_secure=False,
                    region=region,
                    aws_access_key_id=conninfo.userid,
@@ -387,16 +484,23 @@ class Channel(virtual.Channel):
     @property
     def sqs(self):
         if self._sqs is None:
-            self._sqs = self._aws_connect_to(SQSConnection, regions())
+            self._sqs = self._aws_connect_to(SQSConnection, _sqs.regions())
         return self._sqs
 
     @property
-    def asynsqs(self):
-        if self._asynsqs is None:
-            self._asynsqs = self._aws_connect_to(
-                AsyncSQSConnection, _asynsqs.regions(),
-            )
-        return self._asynsqs
+    def sdb(self):
+        if self._sdb is None:
+            self._sdb = self._aws_connect_to(SDBConnection, _sdb.regions())
+        return self._sdb
+
+    @property
+    def table(self):
+        name = self.entity_name(
+            self.domain_format % {'vhost': self.conninfo.virtual_host})
+        d = self.sdb.get_object(
+            'CreateDomain', {'DomainName': name}, self.Table)
+        d.name = name
+        return d
 
     @property
     def conninfo(self):
@@ -417,7 +521,7 @@ class Channel(virtual.Channel):
 
     @cached_property
     def supports_fanout(self):
-        return False
+        return self.transport_options.get('sdb_persistence', False)
 
     @cached_property
     def region(self):
@@ -444,8 +548,3 @@ class Transport(virtual.Transport):
     )
     driver_type = 'emq'
     driver_name = 'emq'
-
-    implements = virtual.Transport.implements.extend(
-        async=True,
-        exchange_type=frozenset(['direct']),
-    )
